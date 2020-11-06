364300690607166197356b6eb8bb136a487c02b9
Safrany Ildiko Csillag <safrild09@gmail.com>
2020-11-06 11:25:13 +0100

    throw

---
+++ Assets/Scripts/RemoveByTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveByTime : MonoBehaviour
+{
+
+    public float timeLimit = 2.0f;
+    public float timeElapsed = 0.0f;
+
+    void OnEnable()
+    {
+        this.timeElapsed = 0.0f;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timeElapsed += Time.deltaTime;
+
+        if (this.timeElapsed > this.timeLimit)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+    }
+}
--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -7,10 +7,20 @@ public class Throw : MonoBehaviour
     public Rigidbody2D candyCaneProto = null;
     public float speed = 20.0f;
      public Transform firePoint;
+     public int itemPoolSize = 10;
+     public List<Rigidbody2D> itemPool;
 
     // Start is called before the first frame update
     void Start()
     {
+        itemPool = new List<Rigidbody2D>();
+        for (int i = 0; i< itemPoolSize; i++)
+        {
+            Rigidbody2D candyCaneClone = Instantiate(candyCaneProto, firePoint.position, candyCaneProto.transform.rotation);
+            candyCaneClone.gameObject.SetActive(false);
+            itemPool.Add(candyCaneClone);
+
+        }
 
 
     }
@@ -27,12 +37,27 @@ public class Throw : MonoBehaviour
 
     void throwCandyCane() 
     {
-        Rigidbody2D candyCaneClone = Instantiate(candyCaneProto, firePoint.position, candyCaneProto.transform.rotation);
+        Rigidbody2D candyCaneClone = getItemFromPool();
+        candyCaneClone.transform.position = firePoint.position;
+
+        candyCaneClone.gameObject.SetActive(true);
 
         float direction = CharacterMovement.facingRight ? +1 : -1;
 
         Vector3 force = transform.right * speed * direction;
         candyCaneClone.velocity = force;
 
+    }
+
+    private Rigidbody2D getItemFromPool(){
+        foreach (Rigidbody2D item in itemPool)
+        {
+            if (!item.gameObject.activeSelf) 
+            {
+                return item;
+            }
+            
+        }
+        return null;
     }
 }
