f007b0159b8f8ae8d54083f66ecd20642f895882
Safrany Ildiko Csillag <safrild09@gmail.com>
2020-11-06 14:24:00 +0100

    tilling parallaxing

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -21,14 +21,14 @@ public class Parallax : MonoBehaviour
         float dist = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
 
-        if (temp > startPosition + length)
+        /*if (temp > startPosition + length)
         {
             startPosition += length;
         }
         else if (temp < startPosition - length)
         {
             startPosition -= length;
-        }
+        }*/
 
     }
 }
