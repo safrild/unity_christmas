43f05bf87ea469e1bff5595e10168599a0a2fc08
Safrany Ildiko Csillag <safrild09@gmail.com>
2020-11-06 13:55:25 +0100

    parallaxing start

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPosition;
+    public GameObject cam;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPosition + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length)
+        {
+            startPosition += length;
+        }
+        else if (temp < startPosition - length)
+        {
+            startPosition -= length;
+        }
+
+    }
+}
